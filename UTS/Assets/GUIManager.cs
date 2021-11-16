4a1efc98bd6e400690cecb5600151f7cf2d8c74d
abel0tax <82127001+abel0tax@users.noreply.github.com>
2021-11-16 12:32:40 +0700

    commit

---
+++ UTS/Assets/GUIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GUIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnPlay() {
+        SceneManager.LoadScene("PLEY!!!");
+    }
+    public void OnCredit() {
+        SceneManager.LoadScene("TUNTANG");
+    }
+    public void OnHelp() {
+        SceneManager.LoadScene("TOLONG");
+    }
+    public void OnMenu() {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
